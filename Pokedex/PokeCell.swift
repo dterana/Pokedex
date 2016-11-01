78e8cbcd0b9dee3bfaf3fd74073f614eeb56e2bb
David TERANA <david.terana@gmail.com>
2016-11-01 17:53:39 -0400

    Add IHM and first iterration of the engine to populate pokemons object with a csv file

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,34 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Pourpre on 11/1/16.
+//  Copyright © 2016 Pourpre. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    
+    func configureCell(pokemon: Pokemon) {
+    
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        
+    }
+    
+}
